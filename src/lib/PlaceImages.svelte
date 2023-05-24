47efd91b01fc3dd9d222da847f1f9096dd44c247
Ian Blake <ian@ianbl8.com>
2023-05-24 12:35:20 +0100

    Move images to new component

---
+++ src/lib/PlaceImages.svelte
@@ -0,0 +1,23 @@
+<script>
+  // @ts-nocheck
+  import { onMount } from "svelte";
+  import { placemarkService } from "../services/placemark-services.js";
+  export let id;
+  
+  let place = [];
+  onMount(async () => {
+    place = await placemarkService.getPlaceById(id);
+  });
+</script>
+
+<h1 class="title is-2"><i class="fas fa-map-marker-alt" style="color:rgb(63, 122, 139)" /> {place.placename}</h1>
+<table class="table is-fullwidth">
+  <thead>
+    <th>Images</th>
+  </thead>
+  <tbody>
+    <tr>
+      <td colspan="4"><img src={place.img} width="600" alt={place.placename} /></td>
+    </tr>
+  </tbody>
+</table>
